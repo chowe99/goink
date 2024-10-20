3a82d07fbda4da9451e834f8e50ca5e9ec0675c9
Chowewow <chowewoow@gmail.com>
2024-10-20 18:43:38 +0800

    added scraper functions

---
+++ scraper/scraper.py
@@ -0,0 +1,33 @@
+import requests
+from bs4 import BeautifulSoup
+from .models import Dataset  # Import the Dataset model
+
+def scrape_data_gov():
+    url = "https://www.data.gov"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    datasets = soup.find_all('a', class_='dataset-heading')
+    for dataset in datasets:
+        title = dataset.get_text().strip()
+        link = dataset['href']
+        description = ""  # Scrape descriptions if necessary
+
+        # Save to the database
+        Dataset.objects.create(title=title, link=link, description=description)
+
+
+def scrape_worldbank():
+    url = "https://data.worldbank.org"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    datasets = soup.find_all('a', class_='indicators-list-item')
+    for dataset in datasets:
+        title = dataset.get_text().strip()
+        link = dataset['href']
+        description = ""  # Scrape descriptions if necessary
+
+        # Save to the database
+        Dataset.objects.create(title=title, link=link, description=description)
+
