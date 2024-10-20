d35922551263347bb6c7a10dcf56ce7a48795624
Chowewow <chowewoow@gmail.com>
2024-10-20 18:44:42 +0800

    scrap view added

--- scraper/views.py
+++ scraper/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
+from .scraper import scrape_data_gov, scrape_worldbank
+
+def scrape_view(request):
+    # Scrape datasets from both sources
+    scrape_data_gov()
+    scrape_worldbank()
+    
+    return HttpResponse("Scraping from data.gov and data.worldbank.org is complete!")
+
 # Create your views here.
+
