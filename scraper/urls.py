85b2d1117f77edc629e683b0da0a03cfd155af52
Chowewow <chowewoow@gmail.com>
2024-10-21 04:30:18 +0800

    Implemented React

--- scraper/urls.py
+++ scraper/urls.py
@@ -3,7 +3,6 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import scrape_view, AirQualityDataAPIView
 router = DefaultRouter()
-router.register(r'datasets', DatasetViewSet, basename='dataset')
 
 urlpatterns = [
     path('scrape/', scrape_view, name='scrape'),
